7b3db30a1d326a641306ba29e9f481e8c8a82d96
nrunde <nikolas.runde@gmail.com>
2018-05-24 08:41:51 +0200

    Added missing content

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,2 @@
+<?php
+$GLOBALS['TL_DCA']['tl_content']['palettes']['fe_mm_import'] = '{type_legend},type,headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
