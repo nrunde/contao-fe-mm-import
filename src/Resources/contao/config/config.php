3a821c764c89af4c1b29a61efa06dc48d10cf331
Nikolas Runde <nikolas.runde@gmail.com>
2018-05-27 15:20:35 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,2 @@
 <?php
-//$GLOBALS['TL_CTE']['ulpr']['fe_mm_import'] = '\Ulpr\ContaoFeMmImportBundle\classes\contao\elements\FeMmImport';
-$GLOBALS['TL_HOOKS']['processFormData'][] = array('FeMmImport','genFeOutput');
+$GLOBALS['TL_HOOKS']['processFormData'][] = array('FeMmImport','importFile');
